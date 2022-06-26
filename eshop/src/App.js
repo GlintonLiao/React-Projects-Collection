8a22b7796879dab6108f1ea4eafb89039a793c59
Guotong Liao <815190191@qq.com>
2022-06-26 12:19:26 +0800

    create header section

--- eshop/src/App.js
+++ eshop/src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
 
-export default App;f
+export default App;
